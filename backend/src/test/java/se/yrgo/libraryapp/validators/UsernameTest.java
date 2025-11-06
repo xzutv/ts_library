e28d002c8e5c50ebcb8a9f6556f36d3bcbb3227b
Haimen <haimenabbas@gmail.com>
2025-11-06 09:47:47 +0100

    added assertJ dependency and changed tests to assertJ format in UserNameTest

--- backend/src/test/java/se/yrgo/libraryapp/validators/UsernameTest.java
+++ backend/src/test/java/se/yrgo/libraryapp/validators/UsernameTest.java
@@ -1,17 +1,21 @@
 package se.yrgo.libraryapp.validators;
 
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UsernameTest {
     @Test
     void correctUsername() {
-        assertTrue(Username.validate("bosse"));
+
+        assertThat(Username.validate("Bosse")).isTrue();
     }
 
     @Test
     void incorrectUsername() {
-        assertFalse(Username.validate("name with space"));
+
+        assertThat(Username.validate("name with space")).isFalse();
     }
 }
